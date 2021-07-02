087eb0d365fc7206f8e0e866394de25ed9b5a8ab
taeyang121096 <taeyang121096@naver.com>
2021-07-02 17:43:30 +0900

    문자열에서 문자 찾기

---
+++ Algorithm/Char_Find.java
@@ -0,0 +1,33 @@
+import java.util.*;
+// 문자 찾기
+public class Char_Find {
+	public int solution(String str, char t) {
+		int answer = 0;
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+		/*
+		for(int i = 0;i<str.length();i++) {
+			if (str.charAt(i) == t) {
+				answer++;
+			}
+		}
+		*/
+		for(char c : str.toCharArray()) {
+			if (c==t) answer++;
+		}
+		return answer;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Char_Find T = new Char_Find();
+		
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		char t = kb.next().charAt(0);
+		System.out.println(T.solution(str, t));
+		
+	}
+
+}
